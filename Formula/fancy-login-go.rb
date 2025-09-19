77d0968613f958488633ee8e7066861663527ba0
Stefan Reinke <stefan.reinke@kuehne-nagel.com>
2025-09-19 09:19:58 +0200

    Update to v1.0.2 with correct SHA256
    - Fix URL to point to v1.0.2 release
    - Update SHA256 to match v1.0.2 tarball
    - Should resolve version parsing and checksum errors

--- Formula/fancy-login-go.rb
+++ Formula/fancy-login-go.rb
@@ -1,8 +1,8 @@
 class FancyLoginGo < Formula
     desc "Fancy AWS/Kubernetes login tool with interactive prompts and k9s integration"
     homepage "https://github.com/reinkes/go-fancy-login"
-    url "https://github.com/reinkes/go-fancy-login/archive/.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    url "https://github.com/reinkes/go-fancy-login/archive/v1.0.2.tar.gz"
+    sha256 "9e9a6aac0c8c8a03a470cad08167756759115610e209a0f3eeab76c0e3d159e9"
     license "MIT"
     head "https://github.com/reinkes/go-fancy-login.git", branch: "main"
 
